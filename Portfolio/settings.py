6116638c4e02cab4dbf4f81227543d4d17452bc2
Shreyansh Upadhyay <upadhyayshreyansh629@gmail.com>
2026-08-07 11:26:17 +0530

    Fix PostgreSQL database

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -83,7 +83,8 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
 
